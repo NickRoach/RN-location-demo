e4f3e552d031ccc87324fcf7f5e5449eda7fe60a
Nick Roach <nickroach@Nicks-MacBook-Pro.local>
2022-02-24 13:34:24 +1100

    works through expo

--- App.tsx
+++ App.tsx
@@ -1,10 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import * as Location from "expo-location";
+import { LocationAccuracy, LocationObject } from "expo-location";
+import React, { useState } from "react";
 
 export default function App() {
+  const [status, requestPermission] = Location.useForegroundPermissions();
+  const [location, setLocation] = useState<LocationObject>();
+
+  requestPermission();
+
+  const handleGetLocation = async () => {
+    console.log(status);
+    const newLocation = await Location.getCurrentPositionAsync({
+      accuracy: LocationAccuracy.High,
+    });
+    setLocation(newLocation);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title="Get location" onPress={handleGetLocation} />
+      <View style={styles.latLong}>
+        <Text>Latitude: {location?.coords.latitude}</Text>
+        <Text>Longitude: {location?.coords.longitude}</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +32,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  latLong: {
+    marginTop: 30,
   },
 });
